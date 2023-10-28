0e13521e27497ac78848ffc00d4951dc221d3638
Andy Freidenfelds <afreidenfelds@gmail.com>
2023-10-28 12:12:39 -0500

    switch to vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -2,32 +2,29 @@ import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
 import { calculateReadingTime } from "./src/utils/readingTime";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://afreidz.codes",
   markdown: {
-    remarkPlugins: [calculateReadingTime],
+    remarkPlugins: [calculateReadingTime]
   },
-  integrations: [
-    mdx(),
-    svelte(),
-    tailwind(),
-    prefetch(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [mdx(), svelte(), tailwind(), prefetch()],
   vite: {
     build: {
       rollupOptions: {
-        external: ["/pagefind/pagefind.js"],
-      },
-    },
+        external: ["/pagefind/pagefind.js"]
+      }
+    }
   },
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
