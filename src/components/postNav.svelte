3114c926aec7b86ddd89e6cb39b4295d251b17b2
Andy Freidenfelds <afreidenfelds@gmail.com>
2023-10-28 13:03:17 -0500

    adds post navigation

---
+++ src/components/postNav.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  let showBackToTop = false;
+
+  function handleScroll() {
+    showBackToTop =
+      document.body.scrollTop > 200 || document.documentElement.scrollTop > 200;
+  }
+
+  $: console.log(showBackToTop);
+
+  function goToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+</script>
+
+<svelte:window on:scroll={handleScroll} />
+
+<div
+  class="lg:sticky fixed bottom-1 right-1 lg:right-auto bg-neutral-200 lg:bg-transparent z-10 rounded-lg flex justify-evenly gap-4 p-4"
+>
+  <a
+    href="#main"
+    on:click|preventDefault={goToTop}
+    class:lg:invisible={!showBackToTop}
+    class:lg:pointer-events-none={!showBackToTop}
+    class="transition-all duration-1000 rounded-full border-2 border-default-100/40 hover:border-accent-100 h-8 w-8 p-1 text-default-100/40 hover:text-accent-100"
+  >
+    <span class="sr-only">Back to top</span>
+    <slot name="top" />
+  </a>
+  <a
+    href="/home"
+    class="transition-colors duration-1000 rounded-full border-2 border-default-100/40 hover:border-accent-100 h-8 w-8 p-1 text-default-100/40 hover:text-accent-100"
+  >
+    <span class="sr-only">Back to home</span>
+    <slot name="home" />
+  </a>
+</div>
