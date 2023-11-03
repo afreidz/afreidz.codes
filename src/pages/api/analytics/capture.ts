598984be13a7bf966b0b9e276fd36d706ba56ed4
Andy Freidenfelds <afreidenfelds@gmail.com>
2023-11-03 15:05:21 -0500

    filtering my known IPs from analytics

--- src/pages/api/analytics/capture.ts
+++ src/pages/api/analytics/capture.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
 import { sql } from "@vercel/postgres";
-import { geolocation } from "@vercel/edge";
+import { geolocation, ipAddress } from "@vercel/edge";
+
+const MYIPS = ["137.184.231.144", "73.110.242.176"];
 
 export const prerender = false;
 export const runtime = "edge";
@@ -8,7 +10,11 @@ export const runtime = "edge";
 export const POST: APIRoute = async ({ request }) => {
   const date = new Date();
   const { slug } = await new Response(request.body).json();
+  const ipaddy = ipAddress(request);
   const { flag, country, city, latitude, longitude } = geolocation(request);
+
+  if (ipaddy && MYIPS.includes(ipaddy))
+    return Response.json({ message: "Not capturing, IP matches known goon!" });
 
   if (!(flag && country && city && latitude && longitude && slug))
     return Response.json({ message: "Missing required parameters" });
