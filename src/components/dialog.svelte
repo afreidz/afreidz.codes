e9f721cc97c23e9883e58a6c37b288fd182e966f
Andy Freidenfelds <afreidenfelds@gmail.com>
2023-11-22 13:36:57 -0600

    updates to post components

--- src/components/dialog.svelte
+++ src/components/dialog.svelte
@@ -1,45 +1,78 @@
 <script lang="ts">
+  import { linear } from "svelte/easing";
   import { createEventDispatcher } from "svelte";
+  import {
+    fade,
+    crossfade,
+    type TransitionConfig,
+    type CrossfadeParams,
+  } from "svelte/transition";
+
+  const [defaultSend, defaultReceive] = crossfade({
+    duration: 0,
+    easing: linear,
+  });
 
   let open = false;
   let customClasses: string = "";
   let dispatch = createEventDispatcher();
+  let key: string = `dialog_${+new Date()}`;
   let elm: HTMLDialogElement | undefined = undefined;
 
-  $: if (open && elm) {
+  let send: (
+    node: any,
+    params: CrossfadeParams & {
+      key: any;
+    },
+  ) => () => TransitionConfig = defaultSend;
+
+  let receive: (
+    node: any,
+    params: CrossfadeParams & {
+      key: any;
+    },
+  ) => () => TransitionConfig = defaultReceive;
+
+  $: if (elm) {
     elm.showModal();
-  } else if (elm) {
-    elm.close();
-    dispatch("close");
   }
 
-  export { elm, open, customClasses as class };
+  $: if (elm && !open) dispatch("close");
+
+  export { elm, open, customClasses as class, key, send, receive };
 </script>
 
-<dialog
-  {open}
-  class:grid={open}
-  bind:this={elm}
-  class="w-full h-full bg-transparent grid-rows-[2.5rem,_auto]"
->
-  <header class="flex justify-end">
-    <button on:click={() => (open = false)} class="w-10 p-2">
-      <i class="h-full">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-          ><path
-            fill="currentColor"
-            d="M18.3 5.71a.996.996 0 0 0-1.41 0L12 10.59L7.11 5.7A.996.996 0 1 0 5.7 7.11L10.59 12L5.7 16.89a.996.996 0 1 0 1.41 1.41L12 13.41l4.89 4.89a.996.996 0 1 0 1.41-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"
-          /></svg
-        >
-      </i>
-    </button>
-  </header>
-  <div class="flex flex-col justify-center h-full {customClasses}">
-    {#if open}
-      <slot />
-    {/if}
-  </div>
-</dialog>
+{#if open}
+  <dialog
+    in:fade
+    out:fade
+    class:grid={open}
+    bind:this={elm}
+    class="w-full h-full bg-transparent grid-rows-[2.5rem,_auto]"
+  >
+    <header class="flex justify-end">
+      <button on:click={() => (open = false)} class="w-10 p-2">
+        <i class="h-full">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+            ><path
+              fill="currentColor"
+              d="M18.3 5.71a.996.996 0 0 0-1.41 0L12 10.59L7.11 5.7A.996.996 0 1 0 5.7 7.11L10.59 12L5.7 16.89a.996.996 0 1 0 1.41 1.41L12 13.41l4.89 4.89a.996.996 0 1 0 1.41-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"
+            /></svg
+          >
+        </i>
+      </button>
+    </header>
+    <div
+      in:send={{ key }}
+      out:receive={{ key }}
+      class="flex flex-col justify-center h-full {customClasses}"
+    >
+      {#if open}
+        <slot />
+      {/if}
+    </div>
+  </dialog>
+{/if}
 
 <style lang="postcss">
   ::backdrop {
--- src/components/post/ytplayer.svelte
+++ src/components/post/ytplayer.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+  import Icon from "@iconify/svelte";
+  import { quintOut } from "svelte/easing";
+  import { crossfade } from "svelte/transition";
   import Dialog from "@/components/dialog.svelte";
+
+  const [send, receive] = crossfade({
+    duration: 1000,
+    easing: quintOut,
+  });
 
   let id: string;
   let open = false;
@@ -8,6 +16,8 @@
 </script>
 
 <a
+  in:send={{ key: id }}
+  out:receive={{ key: id }}
   on:click|preventDefault={() => (open = true)}
   href={`https://www.youtube.com/watch/${id}`}
   class="group relative mx-6 lg:mx-8 rounded-md overflow-hidden flex items-center justify-center aspect-w-16 aspect-h-9"
@@ -19,20 +29,24 @@
   />
   <div class="absolute inset-0 flex items-center justify-center opacity-75">
     <i class="w-16 text-neutral-50">
-      <slot name="icon" />
+      <Icon icon="octicon:play-16" class="w-14 h-14" />
     </i>
   </div>
 </a>
 
-<Dialog {open} on:close={() => (open = false)}>
-  <div class="bg-black w-full aspect-w-16 aspect-h-9 rounded-md overflow-clip">
-    <iframe
-      class="w-full h-full"
-      title="Youtube Video id {id}"
-      src={`https://www.youtube.com/embed/${id}?autoplay=1&playlist=${id}`}
-      allow="accelerometer; autoplay; clipboard-write; loop; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen
-      referrerpolicy="origin"
-    ></iframe>
-  </div>
+<Dialog {send} {receive} key={id} {open} on:close={() => (open = false)}>
+  {#if open}
+    <div
+      class="bg-black w-full aspect-w-16 aspect-h-9 rounded-md overflow-clip"
+    >
+      <iframe
+        class="w-full h-full"
+        allowfullscreen
+        referrerpolicy="origin"
+        title="Youtube Video id {id}"
+        src={`https://www.youtube.com/embed/${id}?autoplay=1&playlist=${id}`}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+      ></iframe>
+    </div>
+  {/if}
 </Dialog>
